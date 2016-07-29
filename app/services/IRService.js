48eed16589882517229dd65ba19e7018b3e70c71
Lais Franco <Lais Franco>
2016-07-29 17:07:53 -0300

    inicio do modulo de ir

---
+++ app/services/IRService.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Lais on 28/07/2016.
+ */
+
+var IRService = {
+
+    calculate:function(salario) {
+
+        if(salario < "1903.98") {
+            return 0;
+        }
+
+    }
+
+
+
+};
+
+module.exports = IRService;
