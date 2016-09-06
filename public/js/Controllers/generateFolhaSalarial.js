e07110f917cc797a28b11a7fe247bf43575ec0bc
isnack <isnack_yagami@hotmail.com>
2016-09-06 05:00:39 -0300

    Front end (#9)
    - Feed Back Usuário
    
    - Atualizar e Cadastrar funcionando
    
    - Gerar Folha de Pagamento em implementaçao...

---
+++ public/js/Controllers/generateFolhaSalarial.js
@@ -0,0 +1,32 @@
+angular.module("folhaPagamento").controller("generateFolhaSalarial",function($scope,$window,funcionarioServices,$location,$timeout){
+     var carregarFolhaSalarial,funcionario,idFuncionario;
+   
+    
+    carregarFolhaSalarial=function(funcionario){
+         funcionarioServices.folhaSalarial(funcionario).success(function (folhaSalarial) {
+         $scope.folhaSalarial =folhaSalarial; 
+         
+			
+		}).error(function (data, status) {
+			$scope.message = "Aconteceu um problema: " + data;
+		});
+        
+    };
+    
+    
+     carregarFuncionario =function(id){  
+      funcionarioServices.getFuncionario(id).success(function (funcionario) {
+       carregarFolhaSalarial(funcionario[0]);
+        
+			
+		}).error(function (data, status) {
+			$scope.message = "Aconteceu um problema: " + data;
+		});
+     };
+    
+    idFuncionario= $location.search().id;  
+    carregarFuncionario(idFuncionario);
+   
+    
+   
+});
