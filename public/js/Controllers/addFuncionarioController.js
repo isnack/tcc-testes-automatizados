647087621a68d494629c18801350acc25afa0a47
Isnack <isnack_yagami@hotmail.com>
2016-09-06 01:53:40 -0300

    Adicionando feedback para o usuário e botão voltar na pagina pagina de cadastro de funcionário

--- public/js/Controllers/addFuncionarioController.js
+++ public/js/Controllers/addFuncionarioController.js
@@ -1,15 +1,21 @@
-angular.module("folhaPagamento").controller("addFuncionarioController",function($scope,funcionarioServices){
+angular.module("folhaPagamento").controller("addFuncionarioController",function($scope,$window,funcionarioServices){
+    $scope.message=null;
     
     $scope.adicionarFuncionario=function(funcionario){
               
         funcionarioServices.addFuncionario(funcionario).success(function(data){
-        delete $scope.funcionario;
-        $scope.funcionarioForm.$setPristine();
+            delete $scope.funcionario;
+            $scope.message="Funcionário adicionado com sucesso";
+            $scope.funcionarioForm.$setPristine();
         }).error(function (data) {
-               $scope.message = "Aconteceu um problema: " + data;
-		});
-      
+              console.log("Aconteceu um problema: " + data);
+               $scope.message = "Não foi possível adicionar funcionário";
+		});              
+    };
+    
+     $scope.voltarPagina =function(){
         
+        $window.location.href="listarFuncionarios.html";
     };
     
 });
