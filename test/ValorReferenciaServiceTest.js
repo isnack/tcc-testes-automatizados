8f15ecab60643c4142441f0d5a85f91d5b22f3bb
Isnack <isnack_yagami@hotmail.com>
2016-10-01 05:32:38 -0300

    testes para o valorReferência refente ao IR

--- test/ValorReferenciaServiceTest.js
+++ test/ValorReferenciaServiceTest.js
@@ -23,6 +23,34 @@ describe('Bateria de testes do serviço de valor referência', function () {
     it('Testando valor refencia do Inss é de 0 %', function () {
         expect(valorReferenciaService.referenciaInss(-2751)).to.equal('0 %');
     });
+    // IR
+
+
+    it('Testando valor refencia do de 7,5&', function () {
+        expect(valorReferenciaService.referenciaIr(2500)).to.equal('7,5 %');
+    });
+
+    it('Testando valor refencia do IR de 15%', function () {
+        expect(valorReferenciaService.referenciaIr(3500)).to.equal('15 %');
+    });
+
+    it('Testando valor refencia do IR de 22,5%', function () {
+        expect(valorReferenciaService.referenciaIr(4500)).to.equal('22,5 %');
+    });
+
+    it('Testando valor refencia do IR de 27,5%', function () {
+        expect(valorReferenciaService.referenciaIr(5500)).to.equal('27,5 %');
+    });
+
+    it('Testando valor refencia do negativo tem que retornar 0', function () {
+        expect(valorReferenciaService.referenciaIr(-1979)).to.equal('0 %');
+
+    });
+
+    it('Testando valor refencia com valores zerados', function () {
+        expect(valorReferenciaService.referenciaIr(0)).to.equal('0 %');
+
+    });
 
 
 });
