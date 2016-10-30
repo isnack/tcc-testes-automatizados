52a964106c5091e6084dd60026e5f0bf9cf38f29
Isnack <isnack_yagami@hotmail.com>
2016-10-30 04:00:28 -0200

    Enviando configurações finais teste funcional e folha de pagamento css

--- testFunctional/functionalTest.js
+++ testFunctional/functionalTest.js
@@ -15,14 +15,72 @@ var funcionario = {
 
 //Objeto contendo teste de inserir e atualizar funcionário autenticação
 var formulario = {
-    testInserirFuncionaorio: function (casper, funcionario) {
+    
+    testAutenticacao:function(casper){
+        casper.start('http://localhost:8585/public/');
+
+        casper.then(function (casper) {
+      this.test.assertExists(x('//*[@id="botao"]'), 'Testando se existe o botão de login');
+        });
+        //Proximo passo é clicar no botão    
+        casper.then(function () {
+            this.click(x('//*[@id="botao"]'));
+        });
+        casper.then(function () {
+            this.capture('testFunctional/img/teste1.png');
+        });
+  // Proximo passo é somente informar o login sem a senha
+        casper.then(function () {
+            this.sendKeys('#usuario', 'admin');
+        });
+        casper.then(function () {
+            this.capture('testFunctional/img/teste2.png');
+        });
+  //Proximo passo é verificar a validação dos campos de autenticação e verificar se foi informado um erro    
+        casper.then(function () {
+            this.test.assertTextExists('Por favor, preencha os campos!', 'Testando se no corpo da pagina contém a frase "Por favor, preencha os campos!"');
+        });
+        casper.then(function () {
+            this.sendKeys('#senha', 'admin1');
+            this.click(x('//*[@id="loginForm"]/div[5]/div/button'));
+        });
+        casper.wait(3000, function () {
+            this.echo("Esperando Resposta do Servidor!!");
+        });
+        casper.then(function () {
+            this.test.assertTextExists('Usuario e/ou senha inválidos', 'Testando se o usuário foi impedido de entrar e se a mensagem de alerta é a correta"');
+        });
+  //Proximo passo informar os campos login e senha corretamente
+        casper.then(function () {
+            this.sendKeys('#usuario', 'admin');
+            this.sendKeys('#senha', 'admin');
+            this.click(x('//*[@id="loginForm"]/div[5]/div/button'));
+        });
+        casper.wait(3000, function () {
+            this.echo("Esperando Entrada no Sistema!");
+        });
+        casper.then(function () {
+            this.capture('testFunctional/img/teste3.png');
+        });
+  //Proximo passo testar se está logado
+        casper.then(function () {
+            this.test.assertTitle("Funcionários", "Testando se está na página de Funcionários");
+
+        });
+    },
+    
+    
+    
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////    
+    
+    testInserirFuncionario: function (casper, funcionario) {
         casper.then(function () {
             this.sendKeys(x('/html/body/div/form/div[1]/input'), funcionario.nome);
         });
 
         casper.then(function () {
 
-            this.capture('img/teste5.png');
+            this.capture('testFunctional/img/teste4.png');
         });
         // Testando validação dos campos
         casper.then(function () {
@@ -40,7 +98,7 @@ var formulario = {
         });
 
         casper.then(function () {
-            this.capture('img/teste5.png');
+            this.capture('testFunctional/img/teste5.png');
 
         });
         casper.wait(2000, function () {
@@ -56,6 +114,8 @@ var formulario = {
         });
 
     },
+    
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
     testAtualizarFuncionario: function (casper) {
         casper.then(function () {
             this.test.assertTextExists('Preencha os campos', 'Testando se no corpo da pagina contém a frase "Por favor, preencha os campos!"');
@@ -69,7 +129,7 @@ var formulario = {
         });
 
         casper.then(function () {
-            this.capture('img/teste7.png');
+            this.capture('testFunctional/img/teste7.png');
 
         });
         casper.wait(2000, function () {
@@ -87,86 +147,35 @@ var formulario = {
             this.echo("Esperando Resposta do Servidor!!");
         });
     },
-    testAutenticacao:function(casper){
-        casper.start('http://localhost:8585/public/');
-
-
-        casper.then(function (casper) {
-      this.test.assertExists(x('//*[@id="botao"]'), 'Testando se existe o botão de login');
-        });
-
-  //Proximo passo é clicar no botão    
-        casper.then(function () {
-            this.click(x('//*[@id="botao"]'));
-        });
-
-        casper.then(function () {
-            this.capture('img/teste2.png');
-        });
-
-  // Proximo passo é somente informar o login sem a senha
-        casper.then(function () {
-            this.sendKeys('#usuario', 'admin');
-        });
-        casper.then(function () {
-            this.capture('img/teste3.png');
-        });
-  //Proximo passo é verificar a validação dos campos de autenticação e verificar se foi informado um erro    
-        casper.then(function () {
-            this.test.assertTextExists('Por favor, preencha os campos!', 'Testando se no corpo da pagina contém a frase "Por favor, preencha     campos!"');
-        });
-
-
-        casper.then(function () {
-            this.sendKeys('#senha', 'admin1');
-            this.click(x('//*[@id="loginForm"]/div[5]/div/button'));
-        });
-
-        casper.wait(3000, function () {
-            this.echo("Esperando Resposta do Servidor!!");
-        });
-        casper.then(function () {
-            this.test.assertTextExists('Usuario e/ou senha inválidos', 'Testando se o usuário foi impedido de entrar e se a mensagem de alerta é a correta"');
-        });
-
-  //Proximo passo informar os campos login e senha corretamente
-
-        casper.then(function () {
-            this.sendKeys('#usuario', 'admin');
-            this.sendKeys('#senha', 'admin');
-            this.click(x('//*[@id="loginForm"]/div[5]/div/button'));
-        });
-
-        casper.wait(3000, function () {
-            this.echo("Esperando Entrada no Sistema!");
-        });
-        casper.then(function () {
-            this.capture('img/teste4.png');
-        });
-
-  //Proximo passo testar se está logado
-        casper.then(function () {
-            this.test.assertTitle("Funcionários", "Testando se está na página de Funcionários");
-
-        });
-    },
-    testGerarFolhaSalarial:function(casper,descontoIr,descontoInss,salario,nome){
+    
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+    
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+    testGerarFolhaSalarial:function(casper,descontoIr,descontoInss,salario,salarioLiquido,nome){
         casper.wait(3000, function () {
             this.echo("Esperando Pagina Carregar Proxima página!!");
         });
      
         casper.then(function () {
-            this.test.assertTextExists(descontoIr, 'Testando o valor de desconto IR está impresso');
+            this.test.assertExists(x("/html/body/table/tbody/tr[3]/td[5][normalize-space()='"+descontoIr+"']"),'Testando o valor de desconto do IR está impresso');
         });
         casper.then(function () {
-            this.capture('img/teste55.png');
-            this.test.assertTextExists(descontoInss, 'Testando o valor de desconto INSS está impresso');
+            this.capture('testFunctional/img/teste6.png');
+            this.test.assertExists(x("/html/body/table/tbody/tr[4]/td[5][normalize-space()='"+descontoInss+"']"),'Testando o valor de desconto do IR está impresso');
         });
-        casper.then(function () {
-            this.test.assertTextExists(salario, 'Testando o valor salario está impresso');
+        casper.then(function(){
+            
+           this.test.assertExists(x("/html/body/table/tbody/tr[2]/td[4][normalize-space()='"+salario+"']"),'Testando o valor salario está impresso');           
         });
+          casper.then(function(){
+            
+           this.test.assertExists(x("/html/body/table/tbody/tr[7]/td[5][normalize-space()='"+salarioLiquido+"']"),'Testando o valor salario Líquido está impresso');           
+        });
+     
         casper.then(function () {
-            this.test.assertTextExists(nome, 'Testando o nome está impresso');
+            this.test.assertExists(x("/html/body/table/thead/tr/th [normalize-space()='Nome: "+nome+"']"),'Testando se o nome do funcionário está impresso');
         });
         
         casper.waitForSelector(x('/html/body/input'), function () {
@@ -180,11 +189,13 @@ var formulario = {
     }
   }
 
-casper.test.begin('Teste Funcional da Folha de Pagamento', 18, function (test) {
-    casper.userAgent('Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)');
+casper.test.begin('Teste Funcional da Folha de Pagamento', 15, function (test) {
+    //casper.userAgent('Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)');
 
+    
+    //Metodo de teste de autenticação de usuário
     formulario.testAutenticacao(casper);
-
+    //Método que retorna a quantidade de tabelas
     casper.then(function () {
         this.getResultsCount = function () {
             return casper.evaluate(function () {
@@ -193,7 +204,7 @@ casper.test.begin('Teste Funcional da Folha de Pagamento', 18, function (test) {
             });
         };
         oldLines = this.getResultsCount();
-        this.capture('img/teste5.png');
+       
     });
     //Entrando na pagina de Cadastro de Funcionários
     casper.then(function () {
@@ -201,7 +212,7 @@ casper.test.begin('Teste Funcional da Folha de Pagamento', 18, function (test) {
     });
 
     //Teste de inserção de funcionário e validação do formulário 
-    formulario.testInserirFuncionaorio(casper, funcionario);
+    formulario.testInserirFuncionario(casper, funcionario);
     casper.wait(3000, function () {
             this.echo("Adição do funcionário!");
      });
@@ -219,7 +230,7 @@ casper.test.begin('Teste Funcional da Folha de Pagamento', 18, function (test) {
 
      });
     
-    formulario.testGerarFolhaSalarial(casper,'27.82','225.00','2500.00','José Luiz');
+    formulario.testGerarFolhaSalarial(casper,'27.82','225.00','2500.00','2247.18','José Luiz');
 
     casper.then(function () {
         this.click(x('//*[@id="tabela' + (this.getResultsCount() - 1) + '"]/tbody/tr[2]/td[3]/button'));
@@ -247,8 +258,9 @@ casper.test.begin('Teste Funcional da Folha de Pagamento', 18, function (test) {
     });
     casper.then(function () {
 
-        this.capture('img/teste5.png');
+        this.capture('testFunctional/img/teste8.png');
     });
+    
     casper.run(function () {
         test.done();
     });
