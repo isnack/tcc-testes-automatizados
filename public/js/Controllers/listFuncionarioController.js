3788b7a9761e6ab9b1b2bf5b51d7dcb761993166
isnack <isnack_yagami@hotmail.com>
2016-11-18 00:50:54 -0200

    Folha pagamento (#26)
    * Criação dos testes funcionais e melhoramento da interface
    
    * Arrumando erros de teste e tentativa de rodar teste funcional no travis
    
    * rest api no travis
    
    * rest api no travis
    
    * rest api no travis2
    
    * rest api no travis3
    
    * rest api no travis4
    
    * rest api no travis6
    
    * rest api no travis7
    
    * rest api no travis7
    
    * rest api no travis8
    
    * Testes funcionais
    
    * Testes funcionais
    
    * Enviando configurações finais teste funcional e folha de pagamento css
    
    * Atualizando reporter

--- public/js/Controllers/listFuncionarioController.js
+++ public/js/Controllers/listFuncionarioController.js
@@ -1,46 +1,47 @@
-angular.module("folhaPagamento").controller("listFuncionarioController",function($scope,$window,funcionarioServices){
-    
-    $scope.funcionarios=[];
-    
-    var carregarFuncionarios =function(){  
+angular.module("folhaPagamento").controller("listFuncionarioController", function ($scope, $window, funcionarioServices) {
+
+    $scope.funcionarios = [];
+
+    var carregarFuncionarios = function () {
         funcionarioServices.getFuncionarios().success(function (data) {
-			$scope.funcionarios = data;
-		}).error(function (data, status) {
-			$scope.message = "Aconteceu um problema: " + data;
-		});
-    
-   };
-    
-     $scope.deleteFuncionario = function(funcionario){
-       /* funcionarioServices.removeFuncionario(funcionario).success(function (data){
-           carregarFuncionarios();
-             $scope.message = "Removido com Sucesso";
-        }).error(function(status){
-            $scope.message = "Aconteceu um problema: " + status;
-            
-        });*/
+            $scope.funcionarios = data;
+        }).error(function (data, status) {
+            $scope.message = "Aconteceu um problema: " + data;
+        });
+
+    };
+
+    $scope.deleteFuncionario = function (funcionario) {
+        funcionarioServices.removeFuncionario(funcionario).success(function (data){
+            carregarFuncionarios();
+              $scope.message = "Removido com Sucesso";
+         }).error(function(status){
+             $scope.message = "Aconteceu um problema: " + status;
+             
+         });
         console.log(funcionario);
     };
-    
-    $scope.abrirAdicionarFuncionario = function(){
-        
-       $window.location.href="cadastroFuncionario.html";
-    };
-    
-     $scope.abrirGerarFolha = function(funcionario){
-        
-       $window.location.href="gerarFolhaSalarial.html#/?id="+funcionario._id;
-    };
-    
-    $scope.abrirAtualizaFuncionario =function(funcionario){
-        
-        $window.location.href="atualizarFuncionario.html#/?id="+funcionario._id;
-    };
-         
-         
-         
-   
-    carregarFuncionarios();
-    
-});
 
+    $scope.abrirAdicionarFuncionario = function () {
+
+        $window.location.href = "cadastroFuncionario.html";
+    };
+
+    $scope.abrirGerarFolha = function (funcionario) {
+
+         $window.location.href="gerarFolhaSalarial.html#/?id="+funcionario._id;
+     
+
+    };
+
+    $scope.abrirAtualizaFuncionario = function (funcionario) {
+
+        $window.location.href = "atualizarFuncionario.html#/?id=" + funcionario._id;
+    };
+
+
+
+
+    carregarFuncionarios();
+
+});
