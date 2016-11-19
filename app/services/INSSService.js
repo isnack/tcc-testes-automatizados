f8b52a87173ba527189f086f7cc7ec9b474ff1b2
Lais Franco <lahh.franco@gmail.com>
2016-11-19 14:43:04 -0200

    refactor do arquivo INSSService.js

--- app/services/INSSService.js
+++ app/services/INSSService.js
@@ -2,13 +2,13 @@ var INSSService = {
     
     calculate:function(sal){
         
-       if(sal >=0 && sal <="1556.94"){
+       if(sal >= 0 && sal <= 1556.94){
            return sal * 0.08;
-       }else if(sal >= "1556.95" && sal <= "2594.92"){
+       }else if(sal >= 1556.95 && sal <= 2594.92){
            return sal * 0.09;
-       }else if(sal>="2594.93" && sal <= "5189.92"){
+       }else if(sal>= 2594.93 && sal <= 5189.92){
            return sal * 0.11;
-       }else if (sal >= "5189.93"){
+       }else if (sal >= 5189.93){
            return 570.88;
        }
         
