523cbf593c08772169e26c450c9e6bf3ad279d60
Isnack <isnack_yagami@hotmail.com>
2016-11-19 23:04:26 -0200

    Alterando serviços IRService.js e ValorReferenciaService.js

--- app/services/IRService.js
+++ app/services/IRService.js
@@ -6,14 +6,14 @@ var IRService = {
 
     calculate:function(salario) {
 
-        if (salario < "1903.98") {
+        if (salario < 1903.98) {
             return 0;
-        } else if (salario >= "1903.99" && salario <= "2826.65") {
+        } else if (salario >= 1903.99 && salario <= 2826.65) {
             return (salario * 0.075) - 142.80;
         }
-        else if (salario >= "2826.66" && salario <= "3751.05") {
+        else if (salario >= 2826.66 && salario <= 3751.05) {
             return (salario * 0.15) - 354.80;
-        } else if (salario >= "3751.06" && salario <= "4664.68") {
+        } else if (salario >= 3751.06 && salario <= 4664.68) {
             return (salario * 0.225) - 636.13;
         } else if (salario >= 4664.69) {
             return (salario * 0.275) - 869.36;
--- app/services/ValorReferenciaService.js
+++ app/services/ValorReferenciaService.js
@@ -6,13 +6,13 @@
 
         referenciaInss:function(salario){
 
-           if(salario >=0 && salario <="1556.94"){
+           if(salario >=0 && salario <= 1556.94){
                return "8 %";
-           }else if(salario >= "1556.95" && salario <= "2594.92"){
+           }else if(salario >= 1556.95 && salario <= 2594.92){
                return "9 %";
-           }else if(salario>="2594.93" && salario <= "5189.92"){
+           }else if(salario>=2594.93 && salario <= 5189.92){
                return "11 %";
-           }else if (salario >= "5189.93"){
+           }else if (salario >= 5189.93){
                return "Teto";
            }
 
@@ -21,14 +21,14 @@
         },
 
         referenciaIr:function(salario){
-            if (salario < "1903.98") {
+            if (salario < 1903.98) {
                 return "0";
-            } else if (salario >= "1903.99" && salario <= "2826.65") {
+            } else if (salario >= 1903.99 && salario <= 2826.65) {
                 return "7,5 %";
             }
-            else if (salario >= "2826.66" && salario <= "3751.05") {
+            else if (salario >= 2826.66 && salario <= 3751.05) {
                 return "15 %";
-            } else if (salario >= "3751.06" && salario <= "4664.68") {
+            } else if (salario >= 3751.06 && salario <= 4664.68) {
                 return "22,5 %";
             } else if (salario >= 4664.69) {
                 return "27,5 %";
